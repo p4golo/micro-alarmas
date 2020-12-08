5be8f6fac23f18fd86fd93bcc2de6c6532707807
Francisco <pagolo2000@gmail.com>
2020-12-08 10:25:19 +0100

    terminado

--- cronometro_alarmas.py
+++ cronometro_alarmas.py
@@ -6,6 +6,11 @@ TERCERACOLUMNA = 3
 DISPLAYAPAGADO = "00000:00000"
 SEGUNDOSALARMA = 0
 MAXALARMAS = 5
+MILISEGUNDOSASEGUNDOS = 1000
+SEGUNDOESPERA = 1
+AUMENTOTIEMPO = 1
+ESPERAALARMA = 5
+CAMBIOTIEMPO = 60
 
 zero = "99999:99999"
 one = "00000:99999"
@@ -21,21 +26,12 @@ nine = "99900:99999"
 fonts = (zero, one, two, three, four, five, six, seven, eight, nine)
 
 def mostrarNumeros(numero,posicion):
-    """
-        Esta funcion muestra en el panel del microbit un numero dado y en la columna seleccionada.
-        @param numero: numero entero de 2 digitos
-        @param posicion: numero de la columna en la que se quiere mostrar el numero.
-    """
     for i in range(FILAS): # Recorre todas las filas y enciende los led de la columna seleccionada con los valores dados
         display.set_pixel(posicion,i,int(numero[0][i]))
     for i in range(FILAS): # Recorre todas las filas y enciende los led de la siguiente columna de la seleccionada con los valores dados
         display.set_pixel(posicion + 1,i,int(numero[1][i]))
 
 def mostrarTiempo(tiempo):
-    """
-        Esta funcion muestra en el panel del microbit el numero de segundos, minutos o segundos que han pasado.
-        @param segundos: numero entero de segundos, minutos o horas que han pasado.
-    """
     if tiempo > 9:
         decenas = tiempo // 10
         decenas = fonts[decenas].split(":")
@@ -48,14 +44,14 @@ def mostrarTiempo(tiempo):
         mostrarNumeros(unidades,TERCERACOLUMNA)
 
 def limpiarDisplay():
-    """
-        Esta funcion apaga todos los pixeles del display,seria un equivalente a display.clear().
-    """
     display_apagado = DISPLAYAPAGADO.split(":")
     mostrarNumeros(display_apagado,PRIMERACOLUMNA)
     mostrarNumeros(display_apagado,TERCERACOLUMNA)
 
 def establecerTiempoAlarma():
+    """
+        Esta funcion establece las horas o los minutos de la alarma y devuelve un entero con su valor.
+    """
     tiempo_alarma = 0
     while not button_b.is_pressed():
         mostrarTiempo(tiempo_alarma)
@@ -66,21 +62,25 @@ def establecerTiempoAlarma():
     return tiempo_alarma
 
 def establecerAlarma(lista):
-    nueva_lista = [(0,0,0)]*(len(lista)+1)
+    """
+        Esta funcion establece la alarma, la introduce en la lista y devuelve la lista.
+        @param lista: una lista de tuplas de 3 posiciones, donde van almacenadas las alarmas.
+    """
+    nueva_lista = [(0,0,0)]*(len(lista)+1) # Creamos una nueva lista de 1 mas de tamaño que la dada
     hora_alarma = establecerTiempoAlarma()
     sleep(1000)
     minuto_alarma = establecerTiempoAlarma()
-    alarma = (hora_alarma,minuto_alarma,SEGUNDOSALARMA)
+    alarma = (hora_alarma,minuto_alarma,SEGUNDOSALARMA) # Creamos la tupla con la alarma que vamos a introducir
     if len(lista) == 0:
         nueva_lista[0] = alarma
     else:
-        for i in range(len(lista)):
+        for i in range(len(lista)): # Recorremos la lista y vamos copiando todos los valores
             nueva_lista[i] = lista[i]
-        nueva_lista[len(lista)] = alarma
+        nueva_lista[len(lista)] = alarma # Añadimos el nuevo valor al final
     return nueva_lista
 
 def alarma():
-    for i in range(5):
+    for i in range(5): # Durante 5 segundos dibuja una cara alegre y otra triste alternativamente
         display.show(Image.HAPPY)
         sleep(500)
         display.clear()
@@ -89,22 +89,29 @@ def alarma():
         display.clear()
 
 def ordenarAlarmas(lista):
-    if len(lista) > 1:
+    """
+        Esta funcion ordena las alarmas de la lista de mas proximas a mas tardias.
+        @param lista: lista de tuplas de 3 posiciones que contiene las alarmas.
+    """
+    if len(lista) > 1: # si el tamaño de la lista es 0 o 1, la lista ya estaria ordenada
         for i in range(len(lista)):
-            for j in range(1,len(lista)):
-                if lista[j] < lista[j-1]:
+            for j in range(1,len(lista)): # Recorre la lista empezando por el segundo elemento
+                if lista[j] < lista[j-1]: # Comprueba si la siguiente alarma es mayor que la actual, si es asi las cambia
                     auxiliar = lista[j]
                     lista[j] = lista[j-1]
                     lista[j-1] = auxiliar
 
 def borrarAlarma(lista):
-    lista_nueva = [(0,0,0)]*(len(lista))
-    for i in range(len(lista)-1):
+    """
+        Esta funcion borra la primera alarma de la lista.
+        @param lista: lista de tuplas de 3 posiciones, que contiene las alarmas.
+    """
+    lista_nueva = [(0,0,0)]*(len(lista)) # Creamos una nueva lista del tamaño de la anterior -1
+    for i in range(len(lista)-1): # Recorremos la lista y vamos rellenandola
         lista_nueva[i] = lista[i+1]
     return lista_nueva
 
 def main():
-
     segundos = 0
     minutos = 0
     horas = 0
@@ -117,10 +124,10 @@ def main():
             else:
                 alarmas_pendientes = establecerAlarma(alarmas_pendientes)
                 ordenarAlarmas(alarmas_pendientes)
-            tiempo = running_time() // 1000
-            tiempo = tiempo - (horas*3600+minutos*60+segundos)
-            if tiempo + segundos >= 60:
-                segundos = (tiempo + segundos) - 60
+            tiempo = running_time() // MILISEGUNDOSASEGUNDOS # Usamos running_time() para comprobar el tiempo que se ha tardado en poner la alarma
+            tiempo = tiempo - (horas*CAMBIOTIEMPO**2+minutos*CAMBIOTIEMPO+segundos)
+            if tiempo + segundos >= CAMBIOTIEMPO: # Sumamos el valor obtenido al tiempo actual
+                segundos = (tiempo + segundos) - CAMBIOTIEMPO
                 minutos += 1
             else:
                 segundos += tiempo
@@ -132,24 +139,23 @@ def main():
             mostrarTiempo(minutos)
             segundos += 1
             sleep(1000)
-        tiempo_actual = (horas,minutos,segundos)
+        tiempo_actual = (horas,minutos,segundos) # Almacenamos en una tupla el tiempo actual para comprobarlo con el de las alarmas
         if len(alarmas_pendientes) > 0:
-            if tiempo_actual == alarmas_pendientes[0]:
+            if tiempo_actual == alarmas_pendientes[0]: # si el tiempo actual es igual a el valor de la primera alarma, ya que estan ordenadas
                 alarma()
-                alarmas_pendientes = borrarAlarma(alarmas_pendientes)
-                segundos += 5
+                alarmas_pendientes = borrarAlarma(alarmas_pendientes) # Obtenemos la nueva lista sin la alarma que acaba de sonar
+                segundos += ESPERAALARMA
         limpiarDisplay()
         mostrarTiempo(segundos)
         sleep(1000) # Para que el bucle tarde un segundo en hacerse
         segundos += 1 # Aumentamos el valor de los segundos
-        if segundos == 60:
+        if segundos == CAMBIOTIEMPO:
             segundos = 0
             minutos += 1
-        if minutos == 60:
+        if minutos == CAMBIOTIEMPO:
             minutos = 0
             horas += 1
         limpiarDisplay()
 
 if __name__ == "__main__":
     main()
-
